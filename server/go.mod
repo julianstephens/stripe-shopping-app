77558dcd238323ef06a6efc543a00c899436ebfe
Julian Stephens <julianstephens55@gmail.com>
2020-10-18 17:58:21 -0400

    Initial commit

---
+++ server/go.mod
@@ -0,0 +1,11 @@
+module github.com/julianstephens/stripe-shopping-app
+
+go 1.15
+
+require (
+	github.com/labstack/echo/v4 v4.1.17
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
+	golang.org/x/net v0.0.0-20201016165138-7b1cca2348c0 // indirect
+	golang.org/x/sys v0.0.0-20201018121011-98379d014ca7 // indirect
+)
---
+++ server/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":8000"))
+}
+
+type createSessionResponse struct {
+	SessionID string `json:"id"`
+}
+
+func createSession(c echo.Context) error {
+
+}
