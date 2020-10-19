4e2b350abd719e3ec51e1fd048fbeb562155f0ff
Julian Stephens <julianstephens55@gmail.com>
2020-10-18 20:43:15 -0400

    Create backend

--- server/go.mod
+++ server/go.mod
@@ -3,8 +3,12 @@ module github.com/julianstephens/stripe-shopping-app
 go 1.15
 
 require (
+	github.com/joho/godotenv v1.3.0
 	github.com/labstack/echo/v4 v4.1.17
 	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/stretchr/testify v1.5.1 // indirect
+	github.com/stripe/stripe-go v70.15.0+incompatible
+	github.com/stripe/stripe-go/v72 v72.16.0
 	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
 	golang.org/x/net v0.0.0-20201016165138-7b1cca2348c0 // indirect
 	golang.org/x/sys v0.0.0-20201018121011-98379d014ca7 // indirect
--- server/main.go
+++ server/main.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"github.com/stripe/stripe-go/v72"
+	"github.com/stripe/stripe-go/v72/checkout/session"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	stripe.Key = os.Getenv("STRIPE_SECRECT_KEY")
+
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+
+	// Routes
+	e.GET("/checkout/success", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Payment successful!")
 	})
+	e.GET("/checkout/cancel", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Payment cancelled.")
+	})
+	e.GET("/create-session", createSession)
+
+	// Start server
 	e.Logger.Fatal(e.Start(":8000"))
 }
 
@@ -19,5 +39,37 @@ type createSessionResponse struct {
 }
 
 func createSession(c echo.Context) error {
+	params := &stripe.CheckoutSessionParams{
+		SuccessURL: stripe.String("/checkout/success"),
+		CancelURL:  stripe.String("/checkout/cancel"),
+		PaymentMethodTypes: stripe.StringSlice([]string{
+			"card",
+		}),
+		LineItems: []*stripe.CheckoutSessionLineItemParams{
+			&stripe.CheckoutSessionLineItemParams{
+				PriceData: &stripe.CheckoutSessionLineItemPriceDataParams{
+					Currency: stripe.String(string(stripe.CurrencyUSD)),
+					ProductData: &stripe.CheckoutSessionLineItemPriceDataProductDataParams{
+						Name: stripe.String("T-shirt"),
+					},
+					UnitAmount: stripe.Int64(2000),
+				},
+				Quantity: stripe.Int64(1),
+			},
+		},
+		Mode: stripe.String(string(stripe.CheckoutSessionModePayment)),
+	}
 
+	session, err := session.New(params)
+
+	if err != nil {
+		log.Printf("session.New: %v", err)
+	}
+
+	data := createSessionResponse{
+		SessionID: session.ID,
+	}
+	resp, _ := json.Marshal(data)
+
+	return c.JSON(http.StatusOK, resp)
 }
